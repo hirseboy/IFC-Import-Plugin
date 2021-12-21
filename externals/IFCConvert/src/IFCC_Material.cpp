b54f23d26480a019b42ed871f3e7ff1f50f9928f
Heiko Fechner <fechner@bauklimatik-fechner.de>
2021-12-21 09:51:21 +0100

    code review, material property handling and writing added

--- externals/IFCConvert/src/IFCC_Material.cpp
+++ externals/IFCConvert/src/IFCC_Material.cpp
@@ -5,7 +5,14 @@
 namespace IFCC {
 
 Material::Material() :
-	m_id(-1)
+	m_id(-1),
+	m_porosity(-1),
+	m_density(-1),
+	m_upperMyValue(-1),
+	m_lowerMyValue(-1),
+	m_moistureDiffusivity(-1),
+	m_heatCapacity(-1),
+	m_thermalConductivity(-1)
 {
 }
 
@@ -25,7 +32,37 @@ TiXmlElement * Material::writeXML(TiXmlElement * parent) const {
 
 	TiXmlElement::appendSingleAttributeElement(e, "Category", nullptr, std::string(), "Miscellaneous");
 
+	if(m_density > 0)
+		TiXmlElement::appendIBKParameterElement(e, "Density", m_densityUnit, m_density);
+	if(m_heatCapacity > 0)
+		TiXmlElement::appendIBKParameterElement(e, "HeatCapacity", m_heatCapacityUnit, m_heatCapacity);
+	if(m_thermalConductivity > 0)
+		TiXmlElement::appendIBKParameterElement(e, "Conductivity", m_thermalConductivityUnit, m_thermalConductivity);
+	if(m_upperMyValue > 0)
+		TiXmlElement::appendIBKParameterElement(e, "Mu", m_upperMyValueUnit, m_upperMyValue);
+	if(m_porosity > 0)
+		TiXmlElement::appendIBKParameterElement(e, "Wsat", m_porosityUnit, m_porosity);
+
 	return e;
 }
+
+void Material::setPropertiesFromPropertyMap(const std::map<std::string,std::map<std::string,Property>>& pmap) {
+	double value = 0;
+	if(getDoubleProperty(pmap,"Pset_MaterialCommon", "Porosity", value))
+		m_porosity = value;
+	if(getDoubleProperty(pmap,"Pset_MaterialCommon", "MassDensity", value))
+		m_density = value;
+	if(getDoubleProperty(pmap,"Pset_MaterialHygroscopic", "UpperVaporResistanceFactor", value))
+		m_upperMyValue = value;
+	if(getDoubleProperty(pmap,"Pset_MaterialHygroscopic", "LowerVaporResistanceFactor", value))
+		m_lowerMyValue = value;
+	if(getDoubleProperty(pmap,"Pset_MaterialHygroscopic", "MoistureDiffusivity", value))
+		m_moistureDiffusivity = value;
+	if(getDoubleProperty(pmap,"Pset_MaterialThermal", "SpecificHeatCapacity", value))
+		m_heatCapacity = value;
+	if(getDoubleProperty(pmap,"Pset_MaterialThermal", "ThermalConductivity", value))
+		m_thermalConductivity = value;
+}
+
 
 } // namespace IFCC
--- externals/IFCConvert/src/IFCC_Material.h
+++ externals/IFCConvert/src/IFCC_Material.h
@@ -3,6 +3,10 @@
 
 #include <tinyxml.h>
 
+#include <map>
+
+#include "IFCC_Property.h"
+
 namespace IFCC {
 
 /*! Class represents a material.*/
@@ -15,11 +19,38 @@ public:
 	/*! Write the component in vicus xml format.*/
 	TiXmlElement * writeXML(TiXmlElement * parent) const;
 
+	/*! Comparison operator.*/
+	friend bool operator==(const Material& left, const Material& right) {
+		if(left.m_name != right.m_name)
+			return false;
+		if(left.m_notes != right.m_notes)
+			return false;
+		return true;
+	}
+
+	void setPropertiesFromPropertyMap(const std::map<std::string,std::map<std::string,Property>>& pmap);
+
 	std::string m_name;				///< Material name
 	int			m_id;				///< Material object id
 	std::string m_notes;			///< Some remarks
 	std::string m_manufacturer;		///< Material producer
 	std::string m_dataSource;		///< Source of material data
+
+	// properties
+	double	m_porosity;
+	double	m_density;
+	double	m_upperMyValue;
+	double	m_lowerMyValue;
+	double	m_moistureDiffusivity;
+	double	m_heatCapacity;
+	double	m_thermalConductivity;
+	std::string	m_porosityUnit = "-";
+	std::string	m_densityUnit = "kg/m3";
+	std::string	m_upperMyValueUnit = "-";
+	std::string	m_lowerMyValueUnit = "-";
+	std::string	m_moistureDiffusivityUnit = "m3/s";
+	std::string	m_heatCapacityUnit = "J/kgK";
+	std::string	m_thermalConductivityUnit = "W/mK";
 };
 
 } // namespace IFCC
