451ae3d8c79add8f587882eabed7e0e74546de4d
Heiko Fechner <fechner@bauklimatik-fechner.de>
2022-02-01 13:07:51 +0100

    some unecessary copies removed

--- bin/release_x64/CreateDeploy_VC14.bat
+++ bin/release_x64/CreateDeploy_VC14.bat
@@ -1,15 +1,9 @@
 @echo off
-set QTDIR=C:\Qt\5.11.3\msvc2015_64
-set PATH=C:\Qt\5.11.3\msvc2015_64\bin;%PATH%
-
-set VC_PATH="c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC"
+set QTDIR=C:\Qt\5.15.2\msvc2019_64
+set PATH=c:\Qt\5.15.2\msvc2019_64\bin;%PATH%
 
 :: setup VC environment variables
-set VCVARSALL_PATH=%VC_PATH%"\vcvarsall.bat"
-call %VCVARSALL_PATH% x64
-
-copy %VC_PATH%"\redist\x64\Microsoft.VC140.CRT\msvcp140.dll" msvcp140.dll
-copy %VC_PATH%"\redist\x64\Microsoft.VC140.CRT\vcruntime140.dll" vcruntime140.dll
-copy %VC_PATH%"\redist\x64\Microsoft.VC140.OpenMP\vcomp140.dll" vcomp140.dll
+set VCVARSALL_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call %VCVARSALL_PATH%
 
 windeployqt.exe IFC2BESTest.exe
