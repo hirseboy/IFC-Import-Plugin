66af5860509752d7c543e8e225b923cd64873584
Heiko Fechner <fechner@bauklimatik-fechner.de>
2023-02-22 14:32:22 +0100

    translation update

--- externals/IFCImportPlugin/resources/translations/ImportIFCPlugin_de.ts
+++ externals/IFCImportPlugin/resources/translations/ImportIFCPlugin_de.ts
@@ -4,8 +4,8 @@
 <context>
     <name>IFCImportPlugin</name>
     <message>
-        <location filename="../../src/IFCImportPlugin.cpp" line="29"/>
         <location filename="../../src/IFCImportPlugin.cpp" line="33"/>
+        <location filename="../../src/IFCImportPlugin.cpp" line="37"/>
         <source>Import IFC file</source>
         <translation>Importiere IFC Datei</translation>
     </message>
@@ -28,32 +28,42 @@
         <translation>Verwende Space Boundaries</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.ui" line="50"/>
+        <location filename="../../src/ImportWPConvert.ui" line="47"/>
         <source>TextLabel</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.ui" line="57"/>
+        <location filename="../../src/ImportWPConvert.ui" line="54"/>
         <source>Repair flags</source>
         <translation>Reparatureinstellungen</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.ui" line="63"/>
+        <location filename="../../src/ImportWPConvert.ui" line="70"/>
         <source>Remove doubled space boundaries</source>
         <translation>Entferne doppelte Space Boundaries</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.ui" line="70"/>
+        <location filename="../../src/ImportWPConvert.ui" line="77"/>
         <source>Flip surface polygons</source>
         <translation>Drehe Polygone</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.ui" line="77"/>
+        <location filename="../../src/ImportWPConvert.ui" line="90"/>
+        <source>Details</source>
+        <translation>Details</translation>
+    </message>
+    <message>
+        <location filename="../../src/ImportWPConvert.ui" line="96"/>
+        <source>More space intersection details</source>
+        <translation>Mehr Informationen zu verschnittenen Räumen</translation>
+    </message>
+    <message>
+        <location filename="../../src/ImportWPConvert.ui" line="60"/>
         <source>If checked all errors will be ignored and the finish button will be enabled</source>
         <translation>Wenn aktiviert, alle Fehler werden ignoriert und die Beenden Schaltfläche wird aktiviert</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.ui" line="80"/>
+        <location filename="../../src/ImportWPConvert.ui" line="63"/>
         <source>Ignore convert errors</source>
         <translation>Ignoriere Konvertierungsfehler</translation>
     </message>
@@ -73,49 +83,91 @@
         <translation>Es gibt %1 Space Boundaries in dieser IFC Datei</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.cpp" line="57"/>
+        <location filename="../../src/ImportWPConvert.cpp" line="71"/>
+        <source>&lt;font color=&quot;#FF0000&quot;&gt;Errors while converting:&lt;/font&gt;</source>
+        <translation>&lt;font color=&quot;#FF0000&quot;&gt;Fehler bei der Konvertierung:&lt;/font&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../src/ImportWPConvert.cpp" line="78"/>
+        <source>&lt;font color=&quot;#FF0000&quot;&gt;Conversion errors:&lt;/font&gt;</source>
+        <translation>&lt;font color=&quot;#FF0000&quot;&gt;Konvertierungsfehler:&lt;/font&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../src/ImportWPConvert.cpp" line="85"/>
+        <source>File converted successfully.</source>
+        <translation>Datei erfolgreich konvertiert.</translation>
+    </message>
+    <message>
+        <location filename="../../src/ImportWPConvert.cpp" line="91"/>
+        <source>&lt;font color=&quot;#FF0000&quot;&gt;Error while converting IFC file.&lt;/font&gt;</source>
+        <translation>&lt;font color=&quot;#FF0000&quot;&gt;Fehler beim Konvrtieren der IFC Datei.&lt;/font&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../src/ImportWPConvert.cpp" line="93"/>
+        <source>%1 spaces intersections found.</source>
+        <translation>%1 verschnittene Räume gefunden.</translation>
+    </message>
+    <message>
+        <location filename="../../src/ImportWPConvert.cpp" line="95"/>
+        <source>Space intersections.&lt;br&gt;</source>
+        <translation>Verschnittene Räume.&lt;br&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../src/ImportWPConvert.cpp" line="99"/>
+        <source>&apos;%1&apos; with &apos;%2&apos;.</source>
+        <translation>&apos;%1&apos; mit &apos;%2&apos;.</translation>
+    </message>
+    <message>
+        <location filename="../../src/ImportWPConvert.cpp" line="104"/>
+        <source>Space %1 with %2 spaces intersected.</source>
+        <translation>Raum %1 ist mit %2 anderen Räumen verschnitten.</translation>
+    </message>
+    <message>
+        <location filename="../../src/ImportWPConvert.cpp" line="109"/>
+        <source>%1 space boundaries with identical surfaces found.</source>
+        <translation>%1 Space Boundaries mit gleichen Flächen gefunden.</translation>
+    </message>
+    <message>
+        <location filename="../../src/ImportWPConvert.cpp" line="112"/>
+        <source>%1 opening space boundaries found which are used more than once in one space.</source>
+        <translation>%1 Space Boundaries für Öffnungen, welche mehr als einmal in einem Raum verwendet wurden, gefunden.</translation>
+    </message>
+    <message>
+        <location filename="../../src/ImportWPConvert.cpp" line="122"/>
         <source>Converting ...</source>
         <translation>Konvertieren ...</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.cpp" line="76"/>
         <source>&lt;font color=&quot;#FF0000&quot;&gt;Errors while converting:&lt;/font&gt;&lt;br&gt;</source>
-        <translation>&lt;font color=&quot;#FF0000&quot;&gt;Fehler beim Konvertieren:&lt;/font&gt;&lt;br&gt;</translation>
+        <translation type="vanished">&lt;font color=&quot;#FF0000&quot;&gt;Fehler beim Konvertieren:&lt;/font&gt;&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.cpp" line="83"/>
         <source>&lt;font color=&quot;#FF0000&quot;&gt;Conversion errors:&lt;/font&gt;&lt;br&gt;</source>
-        <translation>&lt;font color=&quot;#FF0000&quot;&gt;Konvertierungsfehler:&lt;/font&gt;&lt;br&gt;</translation>
+        <translation type="vanished">&lt;font color=&quot;#FF0000&quot;&gt;Konvertierungsfehler:&lt;/font&gt;&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.cpp" line="90"/>
         <source>File converted successfully.&lt;br&gt;</source>
-        <translation>Datei wurde erfolgreich konvertiert.&lt;br&gt;</translation>
+        <translation type="vanished">Datei wurde erfolgreich konvertiert.&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.cpp" line="97"/>
         <source>&lt;font color=&quot;#FF0000&quot;&gt;Error while converting IFC file.&lt;/font&gt;&lt;br&gt;</source>
-        <translation>&lt;font color=&quot;#FF0000&quot;&gt;Fehler beim Konvertieren der IFC Datei.&lt;/font&gt;&lt;br&gt;</translation>
+        <translation type="vanished">&lt;font color=&quot;#FF0000&quot;&gt;Fehler beim Konvertieren der IFC Datei.&lt;/font&gt;&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.cpp" line="99"/>
         <source>%1 spaces intersections found.&lt;br&gt;</source>
-        <translation>%1 verschnittene Räume (Spaces) gefunden.&lt;br&gt;</translation>
+        <translation type="vanished">%1 verschnittene Räume (Spaces) gefunden.&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.cpp" line="101"/>
         <source>Space %1 with %2 spaces intersected.&lt;br&gt;</source>
-        <translation>Raum %1 ist mit %2 weiteren Räumen verschnitten.&lt;br&gt;</translation>
+        <translation type="vanished">Raum %1 ist mit %2 weiteren Räumen verschnitten.&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.cpp" line="105"/>
         <source>%1 space boundaries with identical surfaces found.&lt;br&gt;</source>
-        <translation>%1 Space Boundaries mit identischen Flächen gefunden.&lt;br&gt;</translation>
+        <translation type="vanished">%1 Space Boundaries mit identischen Flächen gefunden.&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/ImportWPConvert.cpp" line="108"/>
         <source>%1 opening space boundaries found which are used more than once in one space.&lt;br&gt;</source>
-        <translation>%1 Space Boundaries für Öffnungen gefunden welche mehr als einmal in einem Raum verwendet werden.&lt;br&gt;</translation>
+        <translation type="vanished">%1 Space Boundaries für Öffnungen gefunden welche mehr als einmal in einem Raum verwendet werden.&lt;br&gt;</translation>
     </message>
 </context>
 <context>
