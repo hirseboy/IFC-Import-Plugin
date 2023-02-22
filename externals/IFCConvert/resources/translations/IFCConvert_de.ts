197c33cb72319646a5713c9847d7ce7d2cab46e3
Heiko Fechner <fechner@bauklimatik-fechner.de>
2023-02-22 14:32:09 +0100

    translation update

--- externals/IFCConvert/resources/translations/IFCConvert_de.ts
+++ externals/IFCConvert/resources/translations/IFCConvert_de.ts
@@ -4,124 +4,124 @@
 <context>
     <name>IFCReader</name>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="440"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="443"/>
         <source>No building site.</source>
         <translation>Keine Baustelle.</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="444"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="447"/>
         <source>No buildings.</source>
         <translation>Kein Gebäude.</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="448"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="451"/>
         <source>No spaces.</source>
         <translation>Keine Räume.</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="592"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="612"/>
         <source>Messages:</source>
         <translation>Nachrichten:</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="611"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="631"/>
         <source>%1 space boundaries.</source>
         <translation>%1 Space Boundaries.</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="613"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="633"/>
         <source>%1 connected with construction elements</source>
         <translation>%1 verbunden mit Konstruktionselementen</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="615"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="635"/>
         <source>%1 connected with opening elements</source>
         <translation>%1 verbunden mit Öffnungselementen</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="617"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="637"/>
         <source>%1 without connection</source>
         <translation>%1 ohne Verbindungen</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="619"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="639"/>
         <source>%1 virtual surfaces</source>
         <translation>%1 virtuelle Flächen</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="629"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="649"/>
         <source>Statistic:&lt;br&gt;</source>
         <translation>Statistik:&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="630"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="650"/>
         <source>%1 buildings.&lt;br&gt;</source>
         <translation>%1 Gebäude.&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="632"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="652"/>
         <source>Building %1 with %2 storeys.&lt;br&gt;</source>
         <translation>Gebäude %1 mit %2 Stockwerken.&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="635"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="655"/>
         <source>  Storey %1 with %2 spaces.&lt;br&gt;</source>
         <translation>  Stockwerk %1 mit %2 Räumen.&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="638"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="658"/>
         <source>    Space %1 with %2 space boundaries.&lt;br&gt;</source>
         <translation>    Raum %1 mit %2 Space Boundaries.&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="643"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="663"/>
         <source>    Space boundary %1 with missing connection.&lt;br&gt;</source>
         <translation>    Space Boundary %1 ohne Verknüpfung.&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="646"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="666"/>
         <source>    Space boundary %1 is virtual.&lt;br&gt;</source>
         <translation>    Space Boundary %1 ist virtuell.&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="649"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="669"/>
         <source>    Space boundary %1 with %2 subsurfaces.&lt;br&gt;</source>
         <translation>    Space Boundary %1 mit %2 Öffnungen.&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="657"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="677"/>
         <source>	%1 materials&lt;br&gt;</source>
         <translation>	%1 Materialien&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="659"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="679"/>
         <source>    %1 - id %2&lt;br&gt;</source>
         <translation>    %1 - Id %2&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="661"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="681"/>
         <source>  %1 constructions&lt;br&gt;</source>
         <translation>  %1 Konstruktionen&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="663"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="683"/>
         <source>    Construction id %1 with %2 layers&lt;br&gt;</source>
         <translation>    Konstruktion Id %1 mit %2 Schichten&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="665"/>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="669"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="685"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="689"/>
         <source>  %1 windows&lt;br&gt;</source>
         <translation>  %1 Fenster&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="667"/>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="671"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="687"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="691"/>
         <source>    Window %1 id %2&lt;br&gt;</source>
         <translation>    Fenster %1 Id %2&lt;br&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/IFCC_IFCReader.cpp" line="674"/>
+        <location filename="../../src/IFCC_IFCReader.cpp" line="694"/>
         <source>Space boundary list &lt;br&gt;</source>
         <translation>Liste der Space Boundaries&lt;br&gt;</translation>
     </message>
