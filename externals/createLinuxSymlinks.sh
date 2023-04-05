8e3df85dbeaca348a02cd07c9be6a783875e4550
Stephan Hirth <stephan.hirth@tu-dresden.de>
2023-04-05 17:17:29 +0200

    Added symlink files

---
+++ externals/createLinuxSymlinks.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Array mit den Ordnernamen
+folders=("IBK" "IBKMK" "QtExt" "TiCPP" "clipper" "lib_x64")
+
+# Zielordner festlegen, in dem der Symlink erstellt werden soll
+target_folder="../externals"
+source_folder="../SIM-VICUS/externals"
+
+for folder in "${folders[@]}"
+do
+  # Prüfen, ob der Ordner bereits existiert
+  if [ -L "$target_folder/$folder" ]
+  then
+    read -p "Der Symlink für den Ordner $folder existiert bereits. Möchten Sie den Ordner löschen und einen neuen Symlink erstellen? (j/n)" choice
+    case "$choice" in
+      j|J )
+        rm "$target_folder/$folder"
+        ln -s "$source_folder/$folder" "$target_folder/$folder"
+        echo "Der Symlink für den Ordner $folder wurde erfolgreich aktualisiert."
+        ;;
+      n|N )
+        echo "Der bestehende Symlink für den Ordner $folder wurde beibehalten."
+        ;;
+      * )
+        echo "Ungültige Eingabe. Der bestehende Symlink für den Ordner $folder wurde beibehalten."
+        ;;
+    esac
+  else
+    # Pfad zum ursprünglichen Ordner oder Datei
+    source_path="$source_folder/$folder"
+
+    # Symlink erstellen
+    ln -s "$source_path" "$target_folder"
+    echo "Der Symlink für den Ordner $folder wurde erfolgreich erstellt."
+  fi
+done
+
---
+++ externals/createWindowsSoftlinks.bat
@@ -0,0 +1,42 @@
+@echo off
+
+goto check_permissions
+
+:create_symlinks
+	set lib_list=IBK lib_x64 QuaZIP QtExt IBKMK sundials SuiteSparse
+
+	(for %%a in (%lib_list%) do (
+	   echo -----------------------------
+	   IF NOT EXIST "%~dp0..\submodules\SIM-VICUS\externals\%%a" (
+		echo Verzeichnis '..\submodules\SIM-VICUS\externals\%%a' existiert nicht. Bitte submodules in git-repository prfen.
+		pause
+		exit /b 1
+	   ) 
+	   if exist %~dp0\%%a (
+			echo L”sche vorhandenen Symlnk der Bibliothek '%%a'
+			rmdir %~dp0\%%a	
+	   )
+	   echo Erzeuge Symlink fr Bibliothek '%%a'
+	   mklink /d %~dp0\%%a %~dp0..\submodules\SIM-VICUS\externals\%%a
+	))
+
+pause
+exit /b 1
+
+@echo off
+goto check_Permissions
+
+:check_permissions
+    echo Adminrechte sind erforderlich! 
+    
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Adminrechte wurden gew„hrt.
+		goto create_symlinks
+    ) else (
+        echo Keine Adminrechte gefunden. Bitte erneut mit Adminrechten ausfhren.
+		pause 
+		exit /b 1
+    )
+    
+    pause >nul
