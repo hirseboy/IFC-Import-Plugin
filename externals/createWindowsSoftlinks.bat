07e4434d7c1a0de5e6eb09306f924d61dce5f476
Stephan Hirth <stephan.hirth@tu-dresden.de>
2026-02-16 14:54:55 +0100

    Updated ifc toolchain

--- externals/createWindowsSoftlinks.bat
+++ externals/createWindowsSoftlinks.bat
@@ -3,40 +3,32 @@
 goto check_permissions
 
 :create_symlinks
-	set lib_list=IBK lib_x64 QtExt IBKMK clipper TiCPP Nandrad Vicus CCM DataIO glm VicOSM QuaZIP HiGHS NandradFMUGenerator
+	set lib_list=IBK QtExt IBKMK clipper TiCPP Nandrad Vicus CCM DataIO glm VicOSM QuaZIP HiGHS NandradFMUGenerator
 
 	(for %%a in (%lib_list%) do (
 	   echo -----------------------------
 	   IF NOT EXIST "%~dp0..\SIM-VICUS\externals\%%a" (
-		echo Verzeichnis '..\SIM-VICUS\externals\%%a' existiert nicht. Bitte submodules in git-repository prfen.
-		pause
+		echo Verzeichnis '..\SIM-VICUS\externals\%%a' existiert nicht. Bitte submodules in git-repository pruefen.
 		exit /b 1
-	   ) 
-	   if exist %~dp0\%%a (
-			echo L”sche vorhandenen Symlnk der Bibliothek '%%a'
-			rmdir %~dp0\%%a	
 	   )
-	   echo Erzeuge Symlink fr Bibliothek '%%a'
+	   if exist %~dp0\%%a (
+			echo Loesche vorhandenen Symlink der Bibliothek '%%a'
+			rmdir %~dp0\%%a
+	   )
+	   echo Erzeuge Symlink fuer Bibliothek '%%a'
 	   mklink /d %~dp0\%%a %~dp0..\SIM-VICUS\externals\%%a
 	))
 
-pause
-exit /b 1
-
-@echo off
-goto check_Permissions
+exit /b 0
 
 :check_permissions
-    echo Adminrechte sind erforderlich! 
-    
+    echo Adminrechte sind erforderlich!
+
     net session >nul 2>&1
     if %errorLevel% == 0 (
-        echo Adminrechte wurden gew„hrt.
+        echo Adminrechte wurden gewaehrt.
 		goto create_symlinks
     ) else (
-        echo Keine Adminrechte gefunden. Bitte erneut mit Adminrechten ausfhren.
-		pause 
+        echo Keine Adminrechte gefunden. Bitte erneut mit Adminrechten ausfuehren.
 		exit /b 1
     )
-    
-    pause >nul
