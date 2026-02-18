9d50b9eec1a8964c8cd6374d1c586a7bc224a34e
Stephan Hirth <stephan.hirth@tu-dresden.de>
2026-02-18 11:25:40 +0100

    Updated symlink bat

--- externals/createWindowsSoftlinks.bat
+++ externals/createWindowsSoftlinks.bat
@@ -3,7 +3,7 @@
 goto check_permissions
 
 :create_symlinks
-	set lib_list=IBK QtExt IBKMK clipper TiCPP Nandrad Vicus CCM DataIO glm VicOSM QuaZIP HiGHS NandradFMUGenerator
+	set lib_list=IBK QtExt IBKMK clipper TiCPP Nandrad Vicus CCM DataIO glm VicOSM QuaZIP HiGHS NandradFMUGenerator lib_x64
 
 	(for %%a in (%lib_list%) do (
 	   echo -----------------------------
@@ -11,12 +11,14 @@ goto check_permissions
 		echo Verzeichnis '..\SIM-VICUS\externals\%%a' existiert nicht. Bitte submodules in git-repository pruefen.
 		exit /b 1
 	   )
-	   if exist %~dp0\%%a (
+	   if exist "%~dp0%%a" (
 			echo Loesche vorhandenen Symlink der Bibliothek '%%a'
-			rmdir %~dp0\%%a
+			:: Try removing as junction first (safe), then as real directory
+			rmdir "%~dp0%%a" 2>nul
+			if exist "%~dp0%%a" rmdir /s /q "%~dp0%%a"
 	   )
 	   echo Erzeuge Symlink fuer Bibliothek '%%a'
-	   mklink /d %~dp0\%%a %~dp0..\SIM-VICUS\externals\%%a
+	   mklink /d "%~dp0%%a" "%~dp0..\SIM-VICUS\externals\%%a"
 	))
 
 	:: NandradCodeGenerator symlink (lives at repo root, not in externals)
@@ -25,19 +27,20 @@ goto check_permissions
 		echo Verzeichnis '..\SIM-VICUS\NandradCodeGenerator' existiert nicht. Bitte submodules in git-repository pruefen.
 		exit /b 1
 	)
-	if exist %~dp0..\NandradCodeGenerator (
+	if exist "%~dp0..\NandradCodeGenerator" (
 		echo Loesche vorhandenen Symlink fuer NandradCodeGenerator
-		rmdir %~dp0..\NandradCodeGenerator
+		rmdir "%~dp0..\NandradCodeGenerator" 2>nul
+		if exist "%~dp0..\NandradCodeGenerator" rmdir /s /q "%~dp0..\NandradCodeGenerator"
 	)
 	echo Erzeuge Symlink fuer NandradCodeGenerator
-	mklink /d %~dp0..\NandradCodeGenerator %~dp0..\SIM-VICUS\NandradCodeGenerator
+	mklink /d "%~dp0..\NandradCodeGenerator" "%~dp0..\SIM-VICUS\NandradCodeGenerator"
 
 exit /b 0
 
 :check_permissions
     echo Adminrechte sind erforderlich!
 
-    net session >nul 2>&1
+    net session >nul 2>nul
     if %errorLevel% == 0 (
         echo Adminrechte wurden gewaehrt.
 		goto create_symlinks
